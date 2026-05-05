d43721036cfeac07b2beffeccc67e47b8d894baa
Sk26102001 <sushantkumar617@gmail.com>
2026-05-05 18:27:42 +0530

    bug fix

--- server/app.js
+++ server/app.js
@@ -21,6 +21,7 @@ app.use(
             'http://127.0.0.1:3000',
             'http://localhost:3000',
             'http://localhost:5173',
+            'https://ekcors-frontend-ochre.vercel.app'
         ],
         credentials: true,
         methods: ['GET', 'POST', 'PATCH', 'DELETE', 'PUT'],
